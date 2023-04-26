1f672442601af4be44e9973198ac418e6d5495da
sakouhichayma3 <sakouhichayma3@gmail.com>
2023-04-26 21:25:07 +0200

    ReactJs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulo from './composant/Formulo';
+import Carous from './composant/Carous';
+function App() {
+  return (
+    <>
+    <Carous/>
+    <Formulo/>
+    <h1>hello word</h1></>
+  );
+}
+
+export default App;
