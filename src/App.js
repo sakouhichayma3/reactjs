d409cc04b0641325a3f27d618f7bec19f150528b
sakouhichayma3 <127346052+sakouhichayma3@users.noreply.github.com>
2023-05-29 21:33:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+{/* importation de stylesheet du boostrap */}
 import 'bootstrap/dist/css/bootstrap.min.css';
+{/* importation de nos composants */}
 import Formulo from './composant/Formulo';
 import Carous from './composant/Carous';
 function App() {
   return (
     <>
+    {/*l'insertionou l'appel de nos composants */}
     <Carous/>
     <Formulo/>
     <h1>hello word</h1></>
